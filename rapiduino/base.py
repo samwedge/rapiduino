1e705938a6e389841c5434a9bd8196fbadd189f6
samwedge <samwedge@gmail.com>
2016-11-22 22:49:45 +0000

    create Pin class

---
+++ rapiduino/base.py
@@ -0,0 +1,19 @@
+from rapiduino.globals import *
+
+
+class Pin(object):
+    def __init__(self):
+        self._pin_mode = INPUT
+
+    @property
+    def pin_mode(self):
+        return self._pin_mode
+
+    @pin_mode.setter
+    def pin_mode(self, value):
+        if value in [INPUT, INPUT_PULLUP, OUTPUT]:
+            self._pin_mode = value
+        else:
+            raise ValueError(
+                'pin_mode must be INPUT, OUTPUT or INPUT_PULLUP but {} was found'.format(value)
+            )
---
+++ rapiduino/tests/test_base.py
@@ -0,0 +1,25 @@
+import unittest
+from rapiduino.base import Pin
+
+
+class TestPin(unittest.TestCase):
+    def setUp(self):
+        self.pin = Pin()
+
+    def test_init(self):
+        self.assertIsInstance(self.pin, Pin)
+        self.assertEqual(self.pin.pin_mode, 'INPUT')
+
+    def test_pin_mode_setget(self):
+        self.pin.pin_mode = 'OUTPUT'
+        self.assertEqual(self.pin.pin_mode, 'OUTPUT')
+        self.pin.pin_mode = 'INPUT'
+        self.assertEqual(self.pin.pin_mode, 'INPUT')
+        self.pin.pin_mode = 'INPUT_PULLUP'
+        self.assertEqual(self.pin.pin_mode, 'INPUT_PULLUP')
+        with self.assertRaises(ValueError):
+            self.pin.pin_mode = 'some_incorrect_value'
+
+
+if __name__ == '__main__':
+    unittest.main()

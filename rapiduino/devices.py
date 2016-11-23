7582d777a691c180086edcbcae6a41013bb9cbfd
samwedge <samwedge@gmail.com>
2016-11-23 22:31:52 +0000

    add readonly pins to Arduino Uno

--- rapiduino/devices.py
+++ rapiduino/devices.py
@@ -1,3 +1,4 @@
+from rapiduino.base import Pin
 
 
 class ArduinoBase(object):
@@ -7,4 +8,29 @@ class ArduinoBase(object):
 class ArduinoUno(ArduinoBase):
 
     def __init__(self):
-        pass
+        self._pins = [
+            Pin(),
+            Pin(),
+            Pin(),
+            Pin(is_pwm=True),
+            Pin(),
+            Pin(is_pwm=True),
+            Pin(is_pwm=True),
+            Pin(),
+            Pin(),
+            Pin(is_pwm=True),
+            Pin(is_pwm=True),
+            Pin(is_pwm=True),
+            Pin(),
+            Pin(),
+            Pin(is_analog=True),
+            Pin(is_analog=True),
+            Pin(is_analog=True),
+            Pin(is_analog=True),
+            Pin(is_analog=True),
+            Pin(is_analog=True),
+        ]
+
+    @property
+    def pins(self):
+        return self._pins[:]
--- rapiduino/tests/test_devices.py
+++ rapiduino/tests/test_devices.py
@@ -2,10 +2,12 @@ import unittest
 
 from rapiduino.devices import ArduinoBase, ArduinoUno
 
+
 class TestArduinoBase(unittest.TestCase):
 
     def test_subclass(self):
         self.assertTrue(issubclass(ArduinoBase, object))
+
 
 class TestArduinoUno(unittest.TestCase):
 
@@ -14,6 +16,15 @@ class TestArduinoUno(unittest.TestCase):
 
     def test_subclass(self):
         self.assertIsInstance(self.device, ArduinoBase)
+
+    def test_number_of_pins(self):
+        self.assertEqual(len(self.device.pins), 20)
+
+    def test_pins_are_readonly(self):
+        with self.assertRaises(AttributeError):
+            self.device.pins = []
+        self.device.pins[0] = ''
+        self.assertNotEqual(self.device.pins[0], '')
 
 
 if __name__ == '__main__':

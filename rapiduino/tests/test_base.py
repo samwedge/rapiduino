4563f5aef6a79d06f265b03b8e33f8fb9aa0096b
samwedge <samwedge@gmail.com>
2016-11-26 22:33:28 +0000

    refector test_base

--- rapiduino/tests/test_base.py
+++ rapiduino/tests/test_base.py
@@ -5,53 +5,53 @@ from rapiduino.base import Pin
 class TestPin(unittest.TestCase):
 
     def setUp(self):
-        self.pin = Pin(5)
+        self.analog_pwm_pin = Pin(0, is_pwm=True, is_analog=True)
+        self.digital_pin = Pin(5)
 
     def test_init(self):
-        self.assertIsInstance(self.pin, Pin)
+        self.assertIsInstance(self.digital_pin, Pin)
         self.assertTrue(issubclass(Pin, object))
 
     def test_defaults(self):
-        self.assertEqual(self.pin.pin_mode, 'INPUT')
-        self.assertEqual(self.pin.pin_state, 'LOW')
-        self.assertEqual(self.pin.is_pwm, False)
-        self.assertEqual(self.pin.is_analog, False)
-        self.assertEqual(self.pin.id, 5)
+        self.assertEqual(self.digital_pin.pin_mode, 'INPUT')
+        self.assertEqual(self.digital_pin.pin_state, 'LOW')
+        self.assertEqual(self.digital_pin.is_pwm, False)
+        self.assertEqual(self.digital_pin.is_analog, False)
+        self.assertEqual(self.digital_pin.id, 5)
 
     def test_pin_mode_setget(self):
-        self.pin.pin_mode = 'OUTPUT'
-        self.assertEqual(self.pin.pin_mode, 'OUTPUT')
-        self.pin.pin_mode = 'INPUT'
-        self.assertEqual(self.pin.pin_mode, 'INPUT')
-        self.pin.pin_mode = 'INPUT_PULLUP'
-        self.assertEqual(self.pin.pin_mode, 'INPUT_PULLUP')
+        self.digital_pin.pin_mode = 'OUTPUT'
+        self.assertEqual(self.digital_pin.pin_mode, 'OUTPUT')
+        self.digital_pin.pin_mode = 'INPUT'
+        self.assertEqual(self.digital_pin.pin_mode, 'INPUT')
+        self.digital_pin.pin_mode = 'INPUT_PULLUP'
+        self.assertEqual(self.digital_pin.pin_mode, 'INPUT_PULLUP')
         with self.assertRaises(ValueError):
-            self.pin.pin_mode = 'some_incorrect_value'
+            self.digital_pin.pin_mode = 'some_incorrect_value'
 
     def test_pin_state_setget(self):
-        self.pin.pin_state = 'HIGH'
-        self.assertEqual(self.pin.pin_state, 'HIGH')
-        self.pin.pin_state = 'LOW'
-        self.assertEqual(self.pin.pin_state, 'LOW')
+        self.digital_pin.pin_state = 'HIGH'
+        self.assertEqual(self.digital_pin.pin_state, 'HIGH')
+        self.digital_pin.pin_state = 'LOW'
+        self.assertEqual(self.digital_pin.pin_state, 'LOW')
         with self.assertRaises(ValueError):
-            self.pin.pin_state = 'some_incorrect_value'
+            self.digital_pin.pin_state = 'some_incorrect_value'
 
     def test_is_pwm_is_readonly(self):
         with self.assertRaises(AttributeError):
-            self.pin.is_pwm = True
+            self.digital_pin.is_pwm = True
 
     def test_is_analog_is_readonly(self):
         with self.assertRaises(AttributeError):
-            self.pin.is_analog = True
+            self.digital_pin.is_analog = True
 
     def test_id_is_readonly(self):
         with self.assertRaises(AttributeError):
-            self.pin.id = 0
+            self.digital_pin.id = 0
 
     def test_pin_preset_overrides(self):
-        analog_pwm_pin = Pin(0, is_pwm=True, is_analog=True)
-        self.assertEqual(analog_pwm_pin.is_analog, True)
-        self.assertEqual(analog_pwm_pin.is_pwm, True)
+        self.assertEqual(self.analog_pwm_pin.is_analog, True)
+        self.assertEqual(self.analog_pwm_pin.is_pwm, True)
 
 
 if __name__ == '__main__':

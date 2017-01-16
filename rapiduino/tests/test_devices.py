50030dee3fa414aeb63ea91c34f7a8a7f079b465
samwedge <samwedge@gmail.com>
2017-01-16 22:53:49 +0000

    refactor tests for ArduinoUno to allow easy testing of new ArduinoBase subclasses

--- rapiduino/tests/test_devices.py
+++ rapiduino/tests/test_devices.py
@@ -6,20 +6,35 @@ from rapiduino.exceptions import PinError
 from rapiduino.globals import *
 
 
-class TestArduinoUno(unittest.TestCase):
+class TestArduinoMixin(object):
 
-    def setUp(self):
-        self.device = ArduinoUno()
+    def setup_mixin(self):
+        self.valid_analog_pin = self.valid_analog_pins[0]
+        self.valid_digital_pin = self.valid_pins[0]
+        self.valid_pwm_pin = self.valid_pwm_pins[0]
+
+        self.num_pins = len(self.valid_pins)
+        self.out_of_range_pin = self.num_pins
+
+        self.invalid_analog_pin = None
+        self.invalid_pwm_pin = None
+        for pin_num in self.valid_pins:
+            if (self.invalid_analog_pin is None) and (pin_num not in self.valid_analog_pins):
+                self.invalid_analog_pin = pin_num
+            if (self.invalid_pwm_pin is None) and (pin_num not in self.valid_pwm_pins):
+                self.invalid_pwm_pin = pin_num
+            if (self.invalid_analog_pin is not None) and (self.invalid_pwm_pin is not None):
+                break
 
     def test_init(self):
         self.assertIsInstance(self.device._pins, tuple)
         self.assertIsInstance(self.device.commands, Commands)
 
     def test_subclass(self):
-        self.assertIsInstance(self.device, ArduinoBase)
+        self.assertTrue(isinstance(self.device, ArduinoBase))
 
     def test_number_of_pins(self):
-        self.assertEqual(len(self.device.pins), 20)
+        self.assertEqual(len(self.device.pins), self.num_pins)
 
     def test_pin_ids(self):
         for pin_no, pin in enumerate(self.device.pins):
@@ -31,92 +46,92 @@ class TestArduinoUno(unittest.TestCase):
         self.assertIsInstance(self.device.pins, tuple)
 
     def test_pin_mode_sets_mode(self):
-        self.device.pin_mode(0, OUTPUT)
-        expected = (10, 0, OUTPUT.value)
+        self.device.pin_mode(self.valid_digital_pin, OUTPUT)
+        expected = (10, self.valid_digital_pin, OUTPUT.value)
         self.assertEqual(self.device.commands.next_command(), expected)
 
     def test_pin_mode_with_incorrect_mode(self):
         with self.assertRaises(ValueError):
-            self.device.pin_mode(0, HIGH)
+            self.device.pin_mode(self.valid_digital_pin, HIGH)
 
     def test_pin_mode_with_pin_no_out_of_range(self):
         with self.assertRaises(IndexError):
-            self.device.pin_mode(500, OUTPUT)
+            self.device.pin_mode(self.out_of_range_pin, OUTPUT)
 
     def test_digital_read_gets_state(self):
-        self.device.digital_read(0)
-        expected = (20, 0)
+        self.device.digital_read(self.valid_digital_pin)
+        expected = (20, self.valid_digital_pin)
         self.assertEqual(self.device.commands.next_command(), expected)
 
     def test_digital_read_with_pin_no_out_of_range(self):
         with self.assertRaises(IndexError):
-            self.device.digital_read(500)
+            self.device.digital_read(self.out_of_range_pin)
 
     def test_digital_write_sets_state(self):
-        self.device.digital_write(0, HIGH)
-        expected = (21, 0, HIGH.value)
+        self.device.digital_write(self.valid_digital_pin, HIGH)
+        expected = (21, self.valid_digital_pin, HIGH.value)
         self.assertEqual(self.device.commands.next_command(), expected)
 
     def test_digital_write_with_incorrect_state(self):
         with self.assertRaises(ValueError):
-            self.device.digital_write(0, OUTPUT)
+            self.device.digital_write(self.valid_digital_pin, OUTPUT)
 
     def test_digital_write_with_pin_no_out_of_range(self):
         with self.assertRaises(IndexError):
-            self.device.digital_write(500, HIGH)
+            self.device.digital_write(self.out_of_range_pin, HIGH)
 
     def test_analog_read_gets_state(self):
-        self.device.analog_read(14)
-        expected = (30, 14)
+        self.device.analog_read(self.valid_analog_pin)
+        expected = (30, self.valid_analog_pin)
         self.assertEqual(self.device.commands.next_command(), expected)
 
     def test_analog_read_with_pin_no_out_of_range(self):
         with self.assertRaises(IndexError):
-            self.device.analog_read(500)
+            self.device.analog_read(self.out_of_range_pin)
 
     def test_analog_read_on_non_analog_pin_raises_error(self):
-        with self.assertRaisesRegexp(PinError, 'cannot complete operation as analog=False for pin 0'):
-            self.device.analog_read(0)
+        with self.assertRaisesRegexp(PinError, 'cannot complete operation as analog=False for pin {}'.format(self.invalid_analog_pin)):
+            self.device.analog_read(self.invalid_analog_pin)
 
     def test_analog_write_sets_state(self):
-        self.device.analog_write(3, 100)
-        expected = (31, 3, 100)
+        self.device.analog_write(self.valid_pwm_pin, 100)
+        expected = (31, self.valid_pwm_pin, 100)
         self.assertEqual(self.device.commands.next_command(), expected)
 
     def test_analog_write_with_incorrect_state(self):
         with self.assertRaises(ValueError):
-            self.device.analog_write(3, 500)
-            self.device.analog_write(3, -1)
+            self.device.analog_write(self.valid_pwm_pin, self.out_of_range_pin)
+            self.device.analog_write(self.valid_pwm_pin, -1)
         with self.assertRaises(TypeError):
-            self.device.analog_write(3, 10.1)
+            self.device.analog_write(self.valid_pwm_pin, 10.1)
 
     def test_analog_write_with_pin_no_out_of_range(self):
         with self.assertRaises(IndexError):
-            self.device.analog_write(500, 100)
+            self.device.analog_write(self.out_of_range_pin, 100)
 
     def test_analog_write_on_non_pwm_pin_raises_error(self):
-        with self.assertRaisesRegexp(PinError, 'cannot complete operation as pwm=False for pin 0'):
-            self.device.analog_write(0, 1)
+        with self.assertRaisesRegexp(PinError, 'cannot complete operation as pwm=False for pin {}'.format(self.invalid_pwm_pin)):
+            self.device.analog_write(self.invalid_pwm_pin, self.invalid_pwm_pin)
 
     def test_assert_valid_pin_number(self):
-        for pin_no in range(20):
+        for pin_no in range(self.num_pins):
             self.device._assert_valid_pin_number(pin_no)
         with self.assertRaises(IndexError):
-            self.device._assert_valid_pin_number(20)
+            self.device._assert_valid_pin_number(self.num_pins)
         with self.assertRaises(IndexError):
             self.device._assert_valid_pin_number(-1)
 
     def test_assert_analog_pin(self):
-        for pin_no in range(14):
-            with self.assertRaisesRegexp(PinError, 'pin {}'.format(pin_no)):
+        for pin_no in self.valid_pins:
+            if pin_no in self.valid_analog_pins:
                 self.device._assert_analog_pin(pin_no)
-        for pin_no in range(14, 20):
-            self.device._assert_analog_pin(pin_no)
+            else:
+                with self.assertRaisesRegexp(PinError, 'pin {}'.format(pin_no)):
+                    self.device._assert_analog_pin(pin_no)
 
     def test_assert_pwm_pin(self):
-        pwm_pins = [3, 5, 6, 9, 10, 11]
-        for pin_no in range(20):
-            if pin_no in pwm_pins:
+        for pin_no in self.valid_pins:
+            if pin_no in self.valid_pwm_pins:
                 self.device._assert_pwm_pin(pin_no)
             else:
                 with self.assertRaisesRegexp(PinError, 'pin {}'.format(pin_no)):
@@ -148,6 +163,17 @@ class TestArduinoUno(unittest.TestCase):
             self.device._assert_valid_pin_state(state)
         with self.assertRaises(ValueError):
             self.device._assert_valid_pin_state(OUTPUT)
+
+
+class TestArduinoUno(unittest.TestCase, TestArduinoMixin):
+
+    def setUp(self):
+        self.device = ArduinoUno()
+        self.valid_pins = range(20)
+        self.valid_analog_pins = range(14, 20)
+        self.valid_pwm_pins = [3, 5, 6, 9, 10, 11]
+
+        self.setup_mixin()
 
 
 if __name__ == '__main__':

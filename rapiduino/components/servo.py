fadc964e249adbb8fd8b6bc67f5364229ab666d5
samwedge <samwedge@gmail.com>
2017-01-31 22:52:38 +0000

    create servo class in components

---
+++ rapiduino/components/servo.py
@@ -0,0 +1,83 @@
+import abc
+import six
+
+from rapiduino.base import Pin
+
+
+@six.add_metaclass(abc.ABCMeta)
+class BaseComponent(object):
+    pass
+
+
+class Servo(BaseComponent):
+
+    def __init__(self, angle_min=0, angle_max=180, pwm_min=0, pwm_max=1023):
+        self._pins = (
+            Pin(0, pwm=True),
+        )
+        self._min_allowed_angle = 0
+        self._max_allowed_angle = 180
+        self._min_allowed_pwm = 0
+        self._max_allowed_pwm = 1023
+
+        self._angle_min = self._min_allowed_angle
+        self._angle_max = self._max_allowed_angle
+        self._pwm_min = self._min_allowed_pwm
+        self._pwm_max = self._max_allowed_pwm
+
+        self.angle_min = angle_min
+        self.angle_max = angle_max
+        self.pwm_min = pwm_min
+        self.pwm_max = pwm_max
+
+    @property
+    def pins(self):
+        return self._pins
+
+    @property
+    def angle_min(self):
+        return self._angle_min
+
+    @angle_min.setter
+    def angle_min(self, value):
+        if value < self._min_allowed_angle:
+            raise ValueError('Minimum value allowed is {}'.format(self._min_allowed_angle))
+        if value >= self._angle_max:
+            raise ValueError('Minimum cannot be >= to the maximum')
+        self._angle_min = value
+
+    @property
+    def angle_max(self):
+        return self._angle_max
+
+    @angle_max.setter
+    def angle_max(self, value):
+        if value > self._max_allowed_angle:
+            raise ValueError('Maximium value allowed is {}'.format(self._max_allowed_angle))
+        if value <= self._angle_min:
+            raise ValueError('Maximum cannot be <= to the minimum')
+        self._angle_max = value
+
+    @property
+    def pwm_min(self):
+        return self._pwm_min
+
+    @pwm_min.setter
+    def pwm_min(self, value):
+        if value < self._min_allowed_pwm:
+            raise ValueError('Minimum value allowed is {}'.format(self._min_allowed_pwm))
+        if value >= self._pwm_max:
+            raise ValueError('Minimum cannot be >= to the maximum')
+        self._pwm_min = value
+
+    @property
+    def pwm_max(self):
+        return self._pwm_max
+
+    @pwm_max.setter
+    def pwm_max(self, value):
+        if value > self._max_allowed_pwm:
+            raise ValueError('Maximium value allowed is {}'.format(self._max_allowed_pwm))
+        if value <= self._pwm_min:
+            raise ValueError('Maximum cannot be <= to the minimum')
+        self._pwm_max = value
---
+++ rapiduino/tests/components/test_servo.py
@@ -0,0 +1,77 @@
+import unittest
+
+from rapiduino.base import Pin
+from rapiduino.components.servo import Servo, BaseComponent
+
+
+class TestServo(unittest.TestCase):
+
+    def setUp(self):
+        self.servo = Servo()
+
+    def test_init(self):
+        self.assertEqual(self.servo.angle_min, 0)
+        self.assertEqual(self.servo.angle_max, 180)
+        self.assertEqual(self.servo.pwm_min, 0)
+        self.assertEqual(self.servo.pwm_max, 1023)
+
+    def test_init_with_invalid_pwm(self):
+        with self.assertRaises(ValueError):
+            Servo(pwm_min=-1)
+        with self.assertRaises(ValueError):
+            Servo(pwm_max=1024)
+        with self.assertRaises(ValueError):
+            Servo(pwm_min=1024)
+        with self.assertRaises(ValueError):
+            Servo(pwm_max=-1)
+
+    def test_init_with_invalid_angle(self):
+        with self.assertRaises(ValueError):
+            Servo(angle_min=-1)
+        with self.assertRaises(ValueError):
+            Servo(angle_max=181)
+        with self.assertRaises(ValueError):
+            Servo(angle_min=181)
+        with self.assertRaises(ValueError):
+            Servo(angle_max=-1)
+
+    def test_set_invalid_pwm(self):
+        with self.assertRaises(ValueError):
+            self.servo.pwm_min = -1
+        with self.assertRaises(ValueError):
+            self.servo.pwm_max = 1024
+        with self.assertRaises(ValueError):
+            self.servo.pwm_min = 1024
+        with self.assertRaises(ValueError):
+            self.servo.pwm_max = -1
+
+    def test_set_invalid_pwm(self):
+        with self.assertRaises(ValueError):
+            self.servo.angle_min = -1
+        with self.assertRaises(ValueError):
+            self.servo.angle_max = 181
+        with self.assertRaises(ValueError):
+            self.servo.angle_min = 181
+        with self.assertRaises(ValueError):
+            self.servo.angle_max = -1
+
+    def test_subclass(self):
+        self.assertTrue(isinstance(self.servo, BaseComponent))
+        for pin in self.servo.pins:
+            self.assertTrue(isinstance(pin, Pin))
+
+    def test_number_of_pins(self):
+        self.assertEqual(len(self.servo.pins), 1)
+
+    def test_pin_ids(self):
+        for pin_no, pin in enumerate(self.servo.pins):
+            self.assertEqual(pin_no, pin.id)
+
+    def test_pins_are_readonly(self):
+        with self.assertRaises(AttributeError):
+            self.servo.pins = []
+        self.assertIsInstance(self.servo.pins, tuple)
+
+
+if __name__ == '__main__':
+    unittest.main()

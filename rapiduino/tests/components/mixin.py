fee116fd3405fc30b0dab03709bb4a8b0f2ed27d
samwedge <samwedge@gmail.com>
2017-02-02 21:55:01 +0000

    Factor out generic tests for subclasses of BaseComponent

---
+++ rapiduino/tests/components/mixin.py
@@ -0,0 +1,26 @@
+from rapiduino.base import Pin
+from rapiduino.components.base import BaseComponent
+
+
+class TestComponentMixin(object):
+
+    def test_subclass(self):
+        self.assertTrue(isinstance(self.component, BaseComponent))
+        for pin in self.component.pins:
+            self.assertTrue(isinstance(pin, Pin))
+
+    def test_number_of_pins(self):
+        self.assertEqual(len(self.component.pins), 1)
+
+    def test_pin_ids(self):
+        for pin_no, pin in enumerate(self.component.pins):
+            self.assertEqual(pin_no, pin.id)
+
+    def test_pins_are_readonly(self):
+        with self.assertRaises(AttributeError):
+            self.component.pins = []
+        self.assertIsInstance(self.component.pins, tuple)
+
+    def test_device_is_readonly(self):
+        with self.assertRaises(AttributeError):
+            self.component.bound_device = 0
--- rapiduino/tests/components/test_servo.py
+++ rapiduino/tests/components/test_servo.py
@@ -1,20 +1,19 @@
 import unittest
 
-from rapiduino.base import Pin
 from rapiduino.components.servo import Servo
-from rapiduino.components.base import BaseComponent
+from rapiduino.tests.components.mixin import TestComponentMixin
 
 
-class TestServo(unittest.TestCase):
+class TestServo(unittest.TestCase, TestComponentMixin):
 
     def setUp(self):
-        self.servo = Servo()
+        self.component = Servo()
 
     def test_init(self):
-        self.assertEqual(self.servo.angle_min, 0)
-        self.assertEqual(self.servo.angle_max, 180)
-        self.assertEqual(self.servo.pwm_min, 0)
-        self.assertEqual(self.servo.pwm_max, 1023)
+        self.assertEqual(self.component.angle_min, 0)
+        self.assertEqual(self.component.angle_max, 180)
+        self.assertEqual(self.component.pwm_min, 0)
+        self.assertEqual(self.component.pwm_max, 1023)
 
     def test_init_with_invalid_pwm(self):
         with self.assertRaises(ValueError):
@@ -38,44 +37,23 @@ class TestServo(unittest.TestCase):
 
     def test_set_invalid_pwm(self):
         with self.assertRaises(ValueError):
-            self.servo.pwm_min = -1
+            self.component.pwm_min = -1
         with self.assertRaises(ValueError):
-            self.servo.pwm_max = 1024
+            self.component.pwm_max = 1024
         with self.assertRaises(ValueError):
-            self.servo.pwm_min = 1024
+            self.component.pwm_min = 1024
         with self.assertRaises(ValueError):
-            self.servo.pwm_max = -1
+            self.component.pwm_max = -1
 
     def test_set_invalid_pwm(self):
         with self.assertRaises(ValueError):
-            self.servo.angle_min = -1
+            self.component.angle_min = -1
         with self.assertRaises(ValueError):
-            self.servo.angle_max = 181
+            self.component.angle_max = 181
         with self.assertRaises(ValueError):
-            self.servo.angle_min = 181
+            self.component.angle_min = 181
         with self.assertRaises(ValueError):
-            self.servo.angle_max = -1
-
-    def test_subclass(self):
-        self.assertTrue(isinstance(self.servo, BaseComponent))
-        for pin in self.servo.pins:
-            self.assertTrue(isinstance(pin, Pin))
-
-    def test_number_of_pins(self):
-        self.assertEqual(len(self.servo.pins), 1)
-
-    def test_pin_ids(self):
-        for pin_no, pin in enumerate(self.servo.pins):
-            self.assertEqual(pin_no, pin.id)
-
-    def test_pins_are_readonly(self):
-        with self.assertRaises(AttributeError):
-            self.servo.pins = []
-        self.assertIsInstance(self.servo.pins, tuple)
-
-    def test_device_is_readonly(self):
-        with self.assertRaises(AttributeError):
-            self.servo.bound_device = 0
+            self.component.angle_max = -1
 
 
 if __name__ == '__main__':

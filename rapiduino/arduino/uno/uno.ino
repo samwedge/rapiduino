3b5f024d85f499bfb0ea2116da6ffd66bfd6f3da
samwedge <samwedge@gmail.com>
2018-04-30 21:24:18 +0100

    Remove Python 3.2 from Travis CI

--- rapiduino/arduino/uno/uno.ino
+++ rapiduino/arduino/uno/uno.ino
@@ -1,33 +1,35 @@
+#include <Servo.h>
+
 /*
- * RAPIDUINO
- * 
- * 
- * For use with the Rapiduino Python Library
- * https://github.com/samwedge/rapiduino
- * 
- * 
- * Copyright (c) 2017 Samuel Wedge
- * samwedge@gmail.com, samwedge.co.uk
- * 
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+   RAPIDUINO
+
+
+   For use with the Rapiduino Python Library
+   https://github.com/samwedge/rapiduino
+
+
+   Copyright (c) 2017 Samuel Wedge
+   samwedge@gmail.com, samwedge.co.uk
+
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
 
 
 char versionMajor = 0;
@@ -38,94 +40,95 @@ char cmdByte;
 char pinNum;
 char dataByte;
 
-void sendByte(char databyte){
+void sendByte(char databyte) {
   Serial.write(databyte);
   return;
 }
 
-char recvByte(){
-  while(!Serial.available());
+char recvByte() {
+  while (!Serial.available());
   return Serial.read();
 }
 
-void setup(){
+void setup() {
   Serial.begin(115200);
 }
 
-void loop(){
+void loop() {
 
   // Receive the Command Byte
   cmdByte = recvByte();
 
   // poll
-  if(cmdByte==0){
+  if (cmdByte == 0) {
     sendByte(1);
   }
 
   // parrot
-  if(cmdByte==1){
+  if (cmdByte == 1) {
     char dataByte = recvByte();
     Serial.write(dataByte);
   }
 
   // version
-  if(cmdByte==2){
+  if (cmdByte == 2) {
     sendByte(versionMajor);
     sendByte(versionMinor);
     sendByte(versionMicro);
   }
 
   // pinMode
-  if(cmdByte==10){
+  if (cmdByte == 10) {
     pinNum = recvByte();
     dataByte = recvByte();
-    if(dataByte == 0){
+    if (dataByte == 0) {
       pinMode(pinNum, INPUT);
     }
-    else if(dataByte == 1){
+    else if (dataByte == 1) {
       pinMode(pinNum, OUTPUT);
     }
-    else if(dataByte == 2){
+    else if (dataByte == 2) {
       pinMode(pinNum, INPUT_PULLUP);
     }
   }
-  
+
   // digitalRead
-  if(cmdByte==20){
+  if (cmdByte == 20) {
     pinNum = recvByte();
     dataByte = digitalRead(pinNum);
-    if(dataByte == LOW){
+    if (dataByte == LOW) {
       Serial.write(0);
     }
-    else if(dataByte == HIGH){
+    else if (dataByte == HIGH) {
       Serial.write(1);
     }
   }
-  
+
   // digitalWrite
-  if(cmdByte==21){
+  if (cmdByte == 21) {
     pinNum = recvByte();
     dataByte = recvByte();
-    if(dataByte == 0){
+    if (dataByte == 0) {
       digitalWrite(pinNum, LOW);
     }
-    else if(dataByte == 1){
+    else if (dataByte == 1) {
       digitalWrite(pinNum, HIGH);
     }
   }
-  
+
   // analogRead
-  if(cmdByte==30){
+  if (cmdByte == 30) {
     pinNum = recvByte();
     dataByte = analogRead(pinNum);
     Serial.write(dataByte);
   }
-  
+
   // analogWrite
-  if(cmdByte==31){;
+  if (cmdByte == 31) {
+    ;
     pinNum = recvByte();
     int value = recvByte();
     analogWrite(pinNum, value);
   }
-   
+
 }

65ed6a778ad79ee6c634d2650fbf50917bd7bd4a
sam.wedge <sam.wedge@autotrader.co.uk>
2021-01-24 20:56:08 +0000

    Add docstrings to exceptions

--- rapiduino/exceptions.py
+++ rapiduino/exceptions.py
@@ -1,34 +1,34 @@
 class RapiduinoError(Exception):
-    pass
+    """Inherited by all rapiduino errors to allow a catch-all"""
 
 
 class SerialConnectionSendDataError(RapiduinoError):
-    pass
+    """Error in transmitting data. Not all sent bytes were reported as being received"""
 
 
 class ReceiveDataSerialConnectionError(RapiduinoError):
-    pass
+    """Received a different number of bytes than specified by the CommandSped"""
 
 
 class NotAnalogPinError(RapiduinoError):
-    pass
+    """This action requires an analog pin, which the specified pin does not support"""
 
 
 class NotPwmPinError(RapiduinoError):
-    pass
+    """This action requires a pwm pin, which the specified pin does not support"""
 
 
 class PinAlreadyRegisteredError(RapiduinoError):
-    pass
+    """The specified pin cannot be registered because it has been registered to a different component"""
 
 
 class ComponentAlreadyRegisteredError(RapiduinoError):
-    pass
+    """The component has already been registered to this board"""
 
 
 class PinDoesNotExistError(RapiduinoError):
-    pass
+    """You have specified a pin that does not exist on this board"""
 
 
 class ProtectedPinError(RapiduinoError):
-    pass
+    """The action cannot be completed because the specified pin is registered to a component"""
